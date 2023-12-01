7bbf8197544e16adecd671fa808b9f99f5dfe400
jessicaavalos <63408895+jessicaavalos@users.noreply.github.com>
2023-12-01 13:56:39 -0500

    Create download.py

---
+++ download.py
@@ -0,0 +1,19 @@
+"""
+Author: Jessica Avalos
+Description: This script facilitates downloading large files by managing the data transfer in segments
+"""
+
+import requests
+
+def download_file(url, local_filename):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=32768):
+                f.write(chunk)
+
+if __name__ == '__main__':
+    dataset_url = 'URL_HERE'
+    local_filename = 'dataset_####.zip'
+
+    download_file(dataset_url, local_filename)
